1da1574db6b8bffcea7a2105a8d9ec9d41c22a5b
Ilya Moroz <ilya.moroz.v@gmail.com>
2024-07-02 07:54:57 -0700

    removed cookies from ctl requests

--- webserver-go/src/packages/proxy/proxy.go
+++ webserver-go/src/packages/proxy/proxy.go
@@ -16,6 +16,15 @@ func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
 	return httputil.NewSingleHostReverseProxy(targetUrl), nil
 }
 
+func removeCookieHeader(original func(*http.Request)) func(*http.Request) {
+	return func(req *http.Request) {
+		original(req)
+		req.Header.Del("Cookie")
+	}
+}
+
 func Register(prefix string, proxy *httputil.ReverseProxy) {
+	proxy.Director = removeCookieHeader(proxy.Director)
+
 	http.HandleFunc(prefix, basicAuth.Middleware(http.StripPrefix(prefix, proxy).ServeHTTP))
 }
